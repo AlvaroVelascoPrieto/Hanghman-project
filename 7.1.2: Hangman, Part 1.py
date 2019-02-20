511880f321c7f2702838b771ea55a2f94c6c0b15
Alvaro Velasco <02alvaro.velasco@gulllakecs.org>
2019-02-20 09:35:23 -0500

    Add the dash count to give a winner

--- 7.1.2: Hangman, Part 1.py
+++ 7.1.2: Hangman, Part 1.py
@@ -11,17 +11,27 @@ def retrieve_guess():
             continue
 
 def update_dashes():
-
     dashes = ""
     for i in range(len(secret_word)):
         dashes = dashes + "-"
     while (1):
         print(dashes)
-        letter = retrieve_guess()
-        for i, word in enumerate(secret_word):
-            if letter == word:
-                dashes = dashes[:i] + letter + dashes[i + 1:]
+        for i, dashcheck in enumerate(dashes):
+            dash_count = 0
+            if dashcheck=="-":
+                dash_count = dash_count + 1
             else:
                 continue
+        if dash_count != 0:
+            letter = retrieve_guess()
+            for i, word in enumerate(secret_word):
+                if letter == word:
+                    dashes = dashes[:i] + letter + dashes[i + 1:]
+                else:
+                    continue
+        else:
+            print("You won!")
+            break
+
 
 update_dashes()
